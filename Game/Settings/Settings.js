3f566cd7972f539786bccc7b7a5a368a3b0919d7
Ambrose McJunkin <jerorfigan@gmail.com>
2016-02-20 23:34:19 -0700

    Make mark graphics scale with board rows/cols

--- Game/Settings/Settings.js
+++ Game/Settings/Settings.js
@@ -19,8 +19,8 @@ if(!window.ttt) window.ttt = {};
 		this.BOARD_FX_LINE_WIDTH_PX = 10;
 		this.BOARD_FX_LINE_STYLE = "#000";
 		this.BOARD_FX_MARK_LINE_STYLE = "#F00";
-		this.BOARD_FX_O_MARK_RADIUS = 60;
-		this.BOARD_FX_X_MARK_RADIUS = 80;
+		this.BOARD_FX_O_MARK_RADIUS = 0.3 * Math.min(this.BOARD_FX_WIDTH_PX / this.BOARD_COLS, this.BOARD_FX_HEIGHT_PX / this.BOARD_ROWS);
+		this.BOARD_FX_X_MARK_RADIUS = 0.4 * Math.min(this.BOARD_FX_WIDTH_PX / this.BOARD_COLS, this.BOARD_FX_HEIGHT_PX / this.BOARD_ROWS);
 		this.BOARD_FX_X_MARK_SIDE_ANGLE = Math.PI/4;
 	};
 
