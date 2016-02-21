9a9e1164c27c91a105f6b344d87fba7ba26c583c
Ambrose McJunkin <jerorfigan@gmail.com>
2016-02-21 13:43:58 -0700

    Add validate, width and height functions

--- Engine/Graphics/BoundingBox.js
+++ Engine/Graphics/BoundingBox.js
@@ -1,7 +1,20 @@
 if(!window.sft) window.sft = {};
 
 (function(){
+	function _validateBoundaryPoints(topLeft, bottomRight){
+		if(
+			topLeft.x() > bottomRight.x() ||
+			topLeft.y() > bottomRight.y()
+		){
+			throw "Invalid boundary points";
+		}
+	}
+
+	/***************/
+	/* Constructor */
+	/***************/
 	var BoundingBox = function(topLeft, bottomRight){
+		_validateBoundaryPoints.call(this, topLeft, bottomRight);
 		this._topLeft = topLeft;
 		this._bottomRight = bottomRight;
 	};
@@ -9,6 +22,9 @@ if(!window.sft) window.sft = {};
 	// Getters / Setters
 	BoundingBox.prototype.topLeft = function(newTopLeft){ if(!!newTopLeft){ this._topLeft = newTopLeft; }else{ return window.sft.util.objects.clone(this._topLeft); }};
 	BoundingBox.prototype.bottomRight = function(newBottomRight) { if(!!newBottomRight){ this._bottomRight = newBottomRight; }else{ return window.sft.util.objects.clone(this._bottomRight); }};
+	
+	BoundingBox.prototype.width = function(){ return this._bottomRight.x() - this._topLeft.x(); };
+	BoundingBox.prototype.height = function(){ return this._bottomRight.y() - this._topLeft.y(); };
 
 	BoundingBox.prototype.toString = function(){ return "Top Left: " + this._topLeft.toString() + " Bottom Right: " + this._bottomRight.toString(); };
 
